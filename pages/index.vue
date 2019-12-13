1f03de03fdfc2a9cb076f757955c625a0a189495
Uthpala Heenatigala <u.heenatigala@temper.works>
2019-12-13 23:36:14 +0100

    lesson 5 graphql queries with arguments

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="container">
     <div>
-      <h1 class="text-4xl font-semibold text-gray-800">
+      <h1 class="text-4xl font-semibold text-gray-800 mb-8">
         DoingITeasyChannel - Nuxt Series
       </h1>
+      <button
+        class="bg-gray-700 p-4 border text-white"
+        @click="characterId = characterId + 1"
+      >
+        Get next character
+      </button>
+      <h3 class="text-2xl">{{ character.id }} {{ character.name }}</h3>
       <ul>
         <li v-for="character in characters.results" :key="character.id">
           {{ character.name }} -
@@ -16,6 +23,11 @@
 <script>
 import gql from 'graphql-tag'
 export default {
+  data() {
+    return {
+      characterId: 1
+    }
+  },
   apollo: {
     characters: gql`
       query getCharacters {
@@ -27,7 +39,22 @@ export default {
           }
         }
       }
-    `
+    `,
+    character: {
+      query: gql`
+        query getCharacter($id: ID) {
+          character(id: $id) {
+            id
+            name
+          }
+        }
+      `,
+      variables() {
+        return {
+          id: this.characterId
+        }
+      }
+    }
   }
 }
 </script>
